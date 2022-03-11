fadf25b169857803ff1f9753bd360f4bf49f7b0b
AventralUni <78443447+AventralUni@users.noreply.github.com>
2022-03-11 07:58:32 +0000

    Main Board & Enemy Test

---
+++ Assets/FlockGlobals.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockGlobals : MonoBehaviour
+{
+    public float DISTANCE = 2.5f;
+    public float flockSpeed = 5f;
+
+    [Range(0f, 1f)] public float cohesiveForce = 1;
+    [Range(0f, 1f)] public float alignForce = 1;
+    [Range(0f, 1f)] public float separationForce = 1;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockAgent : MonoBehaviour
+{
+    FlockGlobals g;
+    Rigidbody rb;
+
+    List<GameObject> getNear()
+    {
+        List<GameObject> others = new List<GameObject>();
+
+        for (int i = 0; i < FindObjectsOfType(typeof(GameObject)).Length; i++)
+        {
+            var other = FindObjectsOfType(typeof(GameObject))[i] as GameObject;
+
+            if (!other.CompareTag("Agent")) continue;
+            if (other == gameObject) continue;
+            if (Vector3.Distance(transform.position, other.transform.position) > g.DISTANCE) continue;
+
+            others.Add(other);
+        }
+
+        return others;
+    }
+
+    void UpdateAI()
+    {
+        var neighbours = getNear();
+
+        if (neighbours.Count <= 0)
+        {
+            return;
+        }
+
+        //align
+        Vector3 avg_heading = Vector3.zero;
+        for (int i = 0; i < neighbours.Count; i++)
+        {
+            var diff = transform.position - neighbours[i].transform.position;
+            if (Mathf.Abs(diff.magnitude) > g.DISTANCE / 2)
+            {
+                continue;
+            }
+
+            var other = neighbours[i];
+            avg_heading += transform.forward;
+        }
+        avg_heading /= neighbours.Count;
+
+        //separation
+        Vector3 avg_diff = Vector3.zero;
+        for (int i = 0; i < neighbours.Count; i++)
+        {
+            var diff = transform.position - neighbours[i].transform.position;
+            if (Mathf.Abs(diff.magnitude) > g.DISTANCE / 2)
+            {
+                continue;
+            }
+
+            avg_diff += diff / diff.sqrMagnitude;
+        }
+        avg_diff /= neighbours.Count;
+
+        //cohesion
+        Vector3 avg_position = Vector3.zero;
+        for (int i = 0; i < neighbours.Count; i++)
+        {
+            var other = neighbours[i];
+            avg_position += other.transform.position;
+        }
+        avg_position /= neighbours.Count;
+        avg_position -= transform.position;
+
+        var align = avg_heading.normalized;
+        var separation = avg_diff.normalized;
+        var cohesion = avg_position.normalized;
+
+        var netSteer =
+            cohesion * g.cohesiveForce +
+            align * g.alignForce +
+            separation * g.separationForce;
+
+        Move(netSteer * g.flockSpeed);
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        g = GameObject.FindGameObjectWithTag("AIControl").GetComponent<FlockGlobals>();
+
+        transform.Rotate(transform.up * Random.Range(0, 360));
+    }
+
+    void Update()
+    {
+        UpdateAI();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, g.DISTANCE);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * .75f);
+    }
+
+    void Move(Vector3 velocity)
+    {
+       // if (gameObject.name == "FLockAI")
+       // {
+       //     print($"up {transform.up} forward {transform.forward} velocity {velocity}");
+       // }
+
+        //rb.velocity = velocity;
+        transform.forward = velocity;
+        transform.position += velocity * Time.deltaTime;
+    }
+}
